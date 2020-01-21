b87905d8010fbd164a277d3bac5115f4d9fe1bc8
mkireva <mariya@kireva.de>
2020-01-21 19:17:02 +0100

    Updates

---
+++ precache-manifest.9cbaa84dbff10ff6a590a18074f5ab86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffd6cbc7ea0d1e9ac62da57c29024c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df516076706fd689b356",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "b98f3b73ca1194aa0001",
+    "url": "/static/js/2.5582daf6.chunk.js"
+  },
+  {
+    "revision": "507a7370b52a72b5f39c2fecbeb4dc40",
+    "url": "/static/js/2.5582daf6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df516076706fd689b356",
+    "url": "/static/js/main.ae237b7b.chunk.js"
+  },
+  {
+    "revision": "9aa2ea1c00037c4e73e3",
+    "url": "/static/js/runtime-main.2b1a243e.js"
+  }
+]);
